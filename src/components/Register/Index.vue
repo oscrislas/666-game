b552be49913d3771a272b3c61d1d8de6c6a1beb6
oliver <oliver@gmail.com>
2020-05-01 02:13:14 -0700

    agrege un pequeño paddin

--- src/components/Register/Index.vue
+++ src/components/Register/Index.vue
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-12">
             <h1>Crear Cuenta</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore culpa placeat exercitationem reiciendis! Porro, doloremque optio magni hic quae voluptatem illum similique obcaecati incidunt exercitationem ut quasi corporis dicta nemo.</p>
+            <p class="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore culpa placeat exercitationem reiciendis! Porro, doloremque optio magni hic quae voluptatem illum similique obcaecati incidunt exercitationem ut quasi corporis dicta nemo.</p>
             <frame>
                 <input type="text" class="col-6 m-1 p-1">
                 <input type="text" class="col-6 m-1 p-1">
@@ -10,7 +10,7 @@
                 <input type="text" class="col-12 m-1 p-1">
                 <input type="text" class="col-12 m-1 p-1">
                 <input type="text" class="col-12 m-1 p-1">
-                <p>
+                <p class="p-2">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat impedit illo maiores, aspernatur voluptas enim ratione doloremque rem eum voluptate, dolorum praesentium quisquam libero eius deserunt at voluptatem corrupti? Obcaecati!
                 </p>
                 <b-button>Register</b-button>
